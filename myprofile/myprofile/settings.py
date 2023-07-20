c4b4f56bf7e93cf5d0278636b5765f6a5190f591
CODDER4149 <amol.d.4149@gmail.com>
2023-07-20 09:29:11 +0530

    add other pages

--- myprofile/myprofile/settings.py
+++ myprofile/myprofile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@uih51&w!x+&@-f3$x87*u#j5z1x6#2y=u-l_=h#!p9s-ukfyg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
 
 
 # Application definition
