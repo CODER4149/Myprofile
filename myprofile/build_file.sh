ccc22e708ef4bc7ab922a2ad40d194240eb8435c
CODDER4149 <amol.d.4149@gmail.com>
2023-08-13 21:20:49 +0530

    add vercel settings

---
+++ myprofile/build_file.sh
@@ -0,0 +1,3 @@
+echo "----- BUILD STRAT -------"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
--- myprofile/myprofile/settings.py
+++ myprofile/myprofile/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-@uih51&w!x+&@-f3$x87*u#j5z1x6#2y=u-l_=h#!p9s-ukfyg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '10.129.2.209']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '10.129.2.209','.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -120,6 +121,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR , 'static')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
